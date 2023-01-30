e00a08d295bbfd54bce7f10d4039fbffe7581946
Yağız <91464589+ktriacxx@users.noreply.github.com>
2023-01-30 19:28:22 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 module.exports = {
-    token:"MTA2Njc5Mjc2NjYzMTMxNzUxNA.GvPwM7.4attvgxMZtLTxY1JfJtAi0TjgQOW9mhAUSz4LU",
-    yönetim:"MTA2Njc5NDg5NjU2MTgwNzUwMA.GmpK1-.Jw2MspXBwNruIqeKOUuepxI7oGLR-YoQ2Xgp44",
-    botID:"1066792766631317514",
-    mongoose:"mongodb://tnx2y:harman@45.200.120.251:27017/Ria?authMechanism=DEFAULT&authSource=admin",
-    guildId:"1048340261325000744",
+    token:"",
+    yönetim:"",
+    botID:"",
+    mongoose:"",
+    guildId:"",
     prefix:".",
-    ses:"1066474718141612132",
-    bio:["ktriadeusa"],
+    ses:"",
+    bio:[""],
     tags: [""],
     name: ""
 }
